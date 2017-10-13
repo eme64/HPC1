bd315a819a376b757e513255d09b8c0cd24f4766
eme64 <emanuel.peter@hotmail.com>
2017-10-13 11:54:22 +0200

    commi 6

--- ex3/mc_omp.cpp
+++ ex3/mc_omp.cpp
@@ -5,7 +5,7 @@
 #include <random>
 // TODO 2: add header for OpenMP library
 // ...
-
+#include <omp.h>
 
 // TODO 3: reimplement using corresponding OpenMP function
 double get_wtime(void)		
@@ -30,27 +30,37 @@ int main(int argc, char *argv[])
 	unsigned long n = 1e8;    // number of samples
 
 	double h = (b-a)/n;
-  double ref = 3.14159265358979323846; 
+	double ref = 3.14159265358979323846; 
 
 	double t0 = get_wtime();
 
 	// http://www.cplusplus.com/reference/random/uniform_real_distribution/
-	std::default_random_engine gen(314); // generator with given seed
-	std::uniform_real_distribution<double> uni(a,b);	
+	//std::default_random_engine gen(314); // generator with given seed
+	//std::uniform_real_distribution<double> uni(a,b);	
 
-  // TODO 4: parallelize using OpenMP
+	// TODO 4: parallelize using OpenMP
 	double res = 0.;
-  double x;
-	for (unsigned long i = 0; i < n; i++) {
-		x = uni(gen);
-		res += f(x);
+	int count_samples = 0;
+	omp_set_num_threads(4);
+	#pragma omp parallel reduction(+:res) reduction(+:count_samples)
+	{
+		printf("id: %d\n", omp_get_thread_num());
+		double x;
+		// init random variable:
+		std::default_random_engine gen(314); // generator with given seed
+		std::uniform_real_distribution<double> uni(a,b);
+		
+		for (unsigned long i = 0; i < n; i++) {
+			x = uni(gen);
+			res += f(x);
+			count_samples += 1;
+		}
+		res *= h;
 	}
-	res *= h;
-
 	double t1 = get_wtime();
 
-	printf("res:  %.16f\nref:  %.16f\nerror: %.5e\ntime: %lf s\n", 
-      res, ref, std::abs(res-ref), t1-t0);
+	printf("res:  %.16f\nref:  %.16f\nerror: %.5e\ntime: %lf s\nnum_samples: %d\nshould num samples: %lu\n", 
+	res, ref, std::abs(res-ref), t1-t0, count_samples, n);
 
 	return 0;
 }
